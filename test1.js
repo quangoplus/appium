3e81cb35c37f9ee0fcd27665151ba8e6b5a9d423
Quan Go Plus <50899795+quangoplus@users.noreply.github.com>
2020-04-03 17:24:27 +0700

    test1.js edited online with GitHub again

--- test1.js
+++ test1.js
@@ -1,9 +1,3 @@
-import * as counter from './test3';
-import * as counter4 from './test4';
-import * as counter5 from './test5';
-const counter6 = require('./test6'); 
-const counter7 = require('./test7'); 
-const counter8 = require('./test8'); 
-
-console.log(counter.counter); //1
+import * as counterz from './test3';
+console.log(counterz.counter); //1
 console.increment();
